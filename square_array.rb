f7ca1a4cc1670b500cf2ee84b98fd468baf39387
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 20:17:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,12 +2,11 @@ def square_array(array)
   
   new_numbers = []
   
-  new_numbers << (num ** 2)
-  
   numbers = [1,2,3]
   
   array.each do |num|
     puts #{num}
+    new_numbers << (num ** 2)
   end
   
   
