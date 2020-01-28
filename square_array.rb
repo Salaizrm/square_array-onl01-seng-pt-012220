b0bdcf9af25b0e11c44db470a21ed22f74269442
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 20:18:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
-def square_array(array)
-  
-  new_numbers = []
-  
-  numbers = [1,2,3]
-  
-  array.each do |num|
-    puts #{num}
+
+
+def square_array(numbers)
+new_numbers = []
+  numbers.each do |num|
     new_numbers << (num ** 2)
   end
-  
-  
-  
+  return new_numbers
 end
